b7d18874bd7ef827e8ed1794dc0c3895de278258
lipan <lipan.me@qq.com>
2022-09-01 20:30:41 +0800

    up

--- zaq-base-redis/src/main/java/com/zaqbest/base/redis/service/config/BaseRedissonConfig.java
+++ zaq-base-redis/src/main/java/com/zaqbest/base/redis/service/config/BaseRedissonConfig.java
@@ -5,6 +5,8 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.Codec;
 import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.ClassUtils;
 
@@ -12,6 +14,8 @@ import org.springframework.util.ClassUtils;
 public class BaseRedissonConfig {
 
     private String address;
+    private String password = null;
+    private int database = 0;
     private int connectionMinimumIdleSize = 10;
     private int idleConnectionTimeout = 10000;
     private int pingTimeout = 1000;
@@ -21,13 +25,11 @@ public class BaseRedissonConfig {
     private int retryInterval = 1500;
     private int reconnectionTimeout = 3000;
     private int failedAttempts = 3;
-    private String password = null;
     private int subscriptionsPerConnection = 5;
     private String clientName = null;
     private int subscriptionConnectionMinimumIdleSize = 1;
     private int subscriptionConnectionPoolSize = 50;
     private int connectionPoolSize = 64;
-    private int database = 0;
     private boolean dnsMonitoring = false;
     private int dnsMonitoringInterval = 5000;
 
@@ -36,9 +38,12 @@ public class BaseRedissonConfig {
     private static final String CODEC = "org.redisson.codec.JsonJacksonCodec";
 
     @Bean(destroyMethod = "shutdown")
+    @ConditionalOnMissingBean(RedissonClient.class)
     RedissonClient redisson() throws Exception {
         Config config = new Config();
-        config.useSingleServer().setAddress(address)
+        config.useSingleServer()
+                .setAddress(address)
+                .setPassword(password)
                 .setConnectionMinimumIdleSize(connectionMinimumIdleSize)
                 .setConnectionPoolSize(connectionPoolSize)
                 .setDatabase(database)
@@ -51,12 +56,171 @@ public class BaseRedissonConfig {
                 .setRetryInterval(retryInterval)
                 .setTimeout(timeout)
                 .setConnectTimeout(connectTimeout)
-                .setIdleConnectionTimeout(idleConnectionTimeout)
-                .setPassword(password);
+                .setIdleConnectionTimeout(idleConnectionTimeout);
         Codec codec = (Codec) ClassUtils.forName(CODEC, ClassUtils.getDefaultClassLoader()).newInstance();
         config.setCodec(codec);
         config.setThreads(thread);
         config.setEventLoopGroup(new NioEventLoopGroup());
         return Redisson.create(config);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(int database) {
+        this.database = database;
+    }
+
+    public int getConnectionMinimumIdleSize() {
+        return connectionMinimumIdleSize;
+    }
+
+    public void setConnectionMinimumIdleSize(int connectionMinimumIdleSize) {
+        this.connectionMinimumIdleSize = connectionMinimumIdleSize;
+    }
+
+    public int getIdleConnectionTimeout() {
+        return idleConnectionTimeout;
+    }
+
+    public void setIdleConnectionTimeout(int idleConnectionTimeout) {
+        this.idleConnectionTimeout = idleConnectionTimeout;
+    }
+
+    public int getPingTimeout() {
+        return pingTimeout;
+    }
+
+    public void setPingTimeout(int pingTimeout) {
+        this.pingTimeout = pingTimeout;
+    }
+
+    public int getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getRetryAttempts() {
+        return retryAttempts;
+    }
+
+    public void setRetryAttempts(int retryAttempts) {
+        this.retryAttempts = retryAttempts;
+    }
+
+    public int getRetryInterval() {
+        return retryInterval;
+    }
+
+    public void setRetryInterval(int retryInterval) {
+        this.retryInterval = retryInterval;
+    }
+
+    public int getReconnectionTimeout() {
+        return reconnectionTimeout;
+    }
+
+    public void setReconnectionTimeout(int reconnectionTimeout) {
+        this.reconnectionTimeout = reconnectionTimeout;
+    }
+
+    public int getFailedAttempts() {
+        return failedAttempts;
+    }
+
+    public void setFailedAttempts(int failedAttempts) {
+        this.failedAttempts = failedAttempts;
+    }
+
+    public int getSubscriptionsPerConnection() {
+        return subscriptionsPerConnection;
+    }
+
+    public void setSubscriptionsPerConnection(int subscriptionsPerConnection) {
+        this.subscriptionsPerConnection = subscriptionsPerConnection;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public int getSubscriptionConnectionMinimumIdleSize() {
+        return subscriptionConnectionMinimumIdleSize;
+    }
+
+    public void setSubscriptionConnectionMinimumIdleSize(int subscriptionConnectionMinimumIdleSize) {
+        this.subscriptionConnectionMinimumIdleSize = subscriptionConnectionMinimumIdleSize;
+    }
+
+    public int getSubscriptionConnectionPoolSize() {
+        return subscriptionConnectionPoolSize;
+    }
+
+    public void setSubscriptionConnectionPoolSize(int subscriptionConnectionPoolSize) {
+        this.subscriptionConnectionPoolSize = subscriptionConnectionPoolSize;
+    }
+
+    public int getConnectionPoolSize() {
+        return connectionPoolSize;
+    }
+
+    public void setConnectionPoolSize(int connectionPoolSize) {
+        this.connectionPoolSize = connectionPoolSize;
+    }
+
+    public boolean isDnsMonitoring() {
+        return dnsMonitoring;
+    }
+
+    public void setDnsMonitoring(boolean dnsMonitoring) {
+        this.dnsMonitoring = dnsMonitoring;
+    }
+
+    public int getDnsMonitoringInterval() {
+        return dnsMonitoringInterval;
+    }
+
+    public void setDnsMonitoringInterval(int dnsMonitoringInterval) {
+        this.dnsMonitoringInterval = dnsMonitoringInterval;
+    }
+
+    public int getThread() {
+        return thread;
+    }
+
+    public void setThread(int thread) {
+        this.thread = thread;
     }
 }
--- zaq-base-redis/src/main/java/com/zaqbest/base/redis/service/impl/RedisServiceImpl.java
+++ zaq-base-redis/src/main/java/com/zaqbest/base/redis/service/impl/RedisServiceImpl.java
@@ -21,197 +21,182 @@ public class RedisServiceImpl implements RedisService {
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
     
-    @Value("${zaq.redis.database:unknown}")
-    private String database;
-    
     @Override
     public void set(String key, Object value, long time) {
-        redisTemplate.opsForValue().set(makeKey(key), value, time, TimeUnit.SECONDS);
+        redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
     }
 
     @Override
     public void set(String key, Object value) {
-        redisTemplate.opsForValue().set(makeKey(key), value);
+        redisTemplate.opsForValue().set(key, value);
     }
 
     @Override
     public Object get(String key) {
-        return redisTemplate.opsForValue().get(makeKey(key));
+        return redisTemplate.opsForValue().get(key);
     }
 
     @Override
     public Boolean del(String key) {
-        return redisTemplate.delete(makeKey(key));
+        return redisTemplate.delete(key);
     }
 
     @Override
     public Long del(List<String> keys) {
-        return redisTemplate.delete(makeKeys(keys));
+        return redisTemplate.delete(keys);
     }
 
     @Override
     public Boolean expire(String key, long time) {
-        return redisTemplate.expire(makeKey(key), time, TimeUnit.SECONDS);
+        return redisTemplate.expire(key, time, TimeUnit.SECONDS);
     }
 
     @Override
     public Long getExpire(String key) {
-        return redisTemplate.getExpire(makeKey(key), TimeUnit.SECONDS);
+        return redisTemplate.getExpire(key, TimeUnit.SECONDS);
     }
 
     @Override
     public Boolean hasKey(String key) {
-        return redisTemplate.hasKey(makeKey(key));
+        return redisTemplate.hasKey(key);
     }
 
     @Override
     public Long incr(String key, long delta) {
-        return redisTemplate.opsForValue().increment(makeKey(key), delta);
+        return redisTemplate.opsForValue().increment(key, delta);
     }
 
     @Override
     public Long decr(String key, long delta) {
-        return redisTemplate.opsForValue().increment(makeKey(key), -delta);
+        return redisTemplate.opsForValue().increment(key, -delta);
     }
 
     @Override
     public Object hGet(String key, String hashKey) {
-        return redisTemplate.opsForHash().get(makeKey(key), hashKey);
+        return redisTemplate.opsForHash().get(key, hashKey);
     }
 
     @Override
     public Boolean hSet(String key, String hashKey, Object value, long time) {
-        redisTemplate.opsForHash().put(makeKey(key), hashKey, value);
+        redisTemplate.opsForHash().put(key, hashKey, value);
         return expire(key, time);
     }
 
     @Override
     public void hSet(String key, String hashKey, Object value) {
-        redisTemplate.opsForHash().put(makeKey(key), hashKey, value);
+        redisTemplate.opsForHash().put(key, hashKey, value);
     }
 
     @Override
     public Map<Object, Object> hGetAll(String key) {
-        return redisTemplate.opsForHash().entries(makeKey(key));
+        return redisTemplate.opsForHash().entries(key);
     }
 
     @Override
     public Boolean hSetAll(String key, Map<String, Object> map, long time) {
-        redisTemplate.opsForHash().putAll(makeKey(key), map);
+        redisTemplate.opsForHash().putAll(key, map);
         return expire(key, time);
     }
 
     @Override
     public void hSetAll(String key, Map<String, ?> map) {
-        redisTemplate.opsForHash().putAll(makeKey(key), map);
+        redisTemplate.opsForHash().putAll(key, map);
     }
 
     @Override
     public void hDel(String key, Object... hashKey) {
-        redisTemplate.opsForHash().delete(makeKey(key), hashKey);
+        redisTemplate.opsForHash().delete(key, hashKey);
     }
 
     @Override
     public Boolean hHasKey(String key, String hashKey) {
-        return redisTemplate.opsForHash().hasKey(makeKey(key), hashKey);
+        return redisTemplate.opsForHash().hasKey(key, hashKey);
     }
 
     @Override
     public Long hIncr(String key, String hashKey, Long delta) {
-        return redisTemplate.opsForHash().increment(makeKey(key), hashKey, delta);
+        return redisTemplate.opsForHash().increment(key, hashKey, delta);
     }
 
     @Override
     public Long hDecr(String key, String hashKey, Long delta) {
-        return redisTemplate.opsForHash().increment(makeKey(key), hashKey, -delta);
+        return redisTemplate.opsForHash().increment(key, hashKey, -delta);
     }
 
     @Override
     public Set<Object> sMembers(String key) {
-        return redisTemplate.opsForSet().members(makeKey(key));
+        return redisTemplate.opsForSet().members(key);
     }
 
     @Override
     public Long sAdd(String key, Object... values) {
-        return redisTemplate.opsForSet().add(makeKey(key), values);
+        return redisTemplate.opsForSet().add(key, values);
     }
 
     @Override
     public Long sAdd(String key, long time, Object... values) {
-        Long count = redisTemplate.opsForSet().add(makeKey(key), values);
+        Long count = redisTemplate.opsForSet().add(key, values);
         expire(key, time);
         return count;
     }
 
     @Override
     public Boolean sIsMember(String key, Object value) {
-        return redisTemplate.opsForSet().isMember(makeKey(key), value);
+        return redisTemplate.opsForSet().isMember(key, value);
     }
 
     @Override
     public Long sSize(String key) {
-        return redisTemplate.opsForSet().size(makeKey(key));
+        return redisTemplate.opsForSet().size(key);
     }
 
     @Override
     public Long sRemove(String key, Object... values) {
-        return redisTemplate.opsForSet().remove(makeKey(key), values);
+        return redisTemplate.opsForSet().remove(key, values);
     }
 
     @Override
     public List<Object> lRange(String key, long start, long end) {
-        return redisTemplate.opsForList().range(makeKey(key), start, end);
+        return redisTemplate.opsForList().range(key, start, end);
     }
 
     @Override
     public Long lSize(String key) {
-        return redisTemplate.opsForList().size(makeKey(key));
+        return redisTemplate.opsForList().size(key);
     }
 
     @Override
     public Object lIndex(String key, long index) {
-        return redisTemplate.opsForList().index(makeKey(key), index);
+        return redisTemplate.opsForList().index(key, index);
     }
 
     @Override
     public Long lPush(String key, Object value) {
-        return redisTemplate.opsForList().rightPush(makeKey(key), value);
+        return redisTemplate.opsForList().rightPush(key, value);
     }
 
     @Override
     public Long lPush(String key, Object value, long time) {
-        Long index = redisTemplate.opsForList().rightPush(makeKey(key), value);
+        Long index = redisTemplate.opsForList().rightPush(key, value);
         expire(key, time);
         return index;
     }
 
     @Override
     public Long lPushAll(String key, Object... values) {
-        return redisTemplate.opsForList().rightPushAll(makeKey(key), values);
+        return redisTemplate.opsForList().rightPushAll(key, values);
     }
 
     @Override
     public Long lPushAll(String key, Long time, Object... values) {
-        Long count = redisTemplate.opsForList().rightPushAll(makeKey(key), values);
-        expire(makeKey(key), time);
+        Long count = redisTemplate.opsForList().rightPushAll(key, values);
+        expire(key, time);
         return count;
     }
 
     @Override
     public Long lRemove(String key, long count, Object value) {
-        return redisTemplate.opsForList().remove(makeKey(key), count, value);
+        return redisTemplate.opsForList().remove(key, count, value);
     }
     
-    private String makeKey(String key){
-        return StrUtil.format("{}:{}", database, key);
-    }
-
-    private List<String> makeKeys(List<String> keys){
-        if (CollUtil.isEmpty(keys)) {
-            return keys;
-        }
-        List<String> ans = new ArrayList<>();
-        keys.stream().forEach(x->ans.add(StrUtil.format("{}:{}", database, x)));
-        return ans;
-    }
 }
