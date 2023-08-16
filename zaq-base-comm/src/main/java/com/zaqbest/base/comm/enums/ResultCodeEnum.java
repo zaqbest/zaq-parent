938936fd9761be7fd7aaa4d2888d5cc06d7de898
lipan <lipan.me@qq.com>
2023-08-16 15:42:32 +0800

    up

--- zaq-base-comm/src/main/java/com/zaqbest/base/comm/enums/ResultCodeEnum.java
+++ zaq-base-comm/src/main/java/com/zaqbest/base/comm/enums/ResultCodeEnum.java
@@ -1,23 +1,15 @@
 package com.zaqbest.base.comm.enums;
 
 public enum ResultCodeEnum implements IErrorCode {
-    /*** 通用部分 100 - 599***/
-    // 成功请求
     SUCCESS(200, "Success"),
     BAD_REQUEST(400, "Bad Request"),
     UNAUTHORIZED(401, "Unauthorized"),
     FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not Found"),
+    TOO_MANY_REQUEST(429, "Too Many Request"),
     INTERNAL_ERROR(500, "Internal Server Error"),
     BAD_GATEWAY(502, "Bad Gateway"),
     UNAVAILABLE(503, "Service Unavailable"),
-
-    /*** 这里可以根据不同模块用不同的区级分开错误码，例如:  ***/
-
-    // 1000～1999 区间表示用户模块错误
-    // 2000～2999 区间表示订单模块错误
-    // 3000～3999 区间表示商品模块错误
-    // 。。。
     ;
     /**
      * 响应状态码
