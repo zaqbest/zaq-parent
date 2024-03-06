9993cd829e3040c36fcde7c4900f780def4b4dc9
lipan <lipan.me@qq.com>
2024-03-06 17:11:20 +0800

    up

--- doc/images/openjdk8-alpine-3.18/docker_push.sh
+++ doc/images/openjdk8-alpine-3.18/docker_push.sh
@@ -1,24 +1,43 @@
 #!env bash
 
-export DOCKER_REGISTRY_URL=${DEV_DOCKER_REGISTRY_URL}
-export DOCKER_PROJECT=${DEV_DOCKER_PROJECT}
 # 配置正确的用户名密码变量
-export DOCKER_USERNAME=${DEV_DOCKER_REGISTRY_USERNAME}
-export DOCKER_PASSWORD=${DEV_DOCKER_REGISTRY_PASSWORD}
+export DOCKER_REGISTRY_URL=${DEV_DOCKER_REGISTRY_URL}
+export DOCKER_REGISTRY_PROJECT=${DEV_DOCKER_REGISTRY_PROJECT}
+export DOCKER_REGISTRY_USERNAME=${DEV_DOCKER_REGISTRY_USERNAME}
+export DOCKER_REGISTRY_PASSWORD=${DEV_DOCKER_REGISTRY_PASSWORD}
 
-export IMAGE_ID=$DOCKER_REGISTRY_URL/$DOCKER_PROJECT/openjdk8:1.0.0-alpine-3.18
-export PLATFROM=linux/amd64,linux/arm64
+export PLATFORMS=${PLATFORMS:=linux/amd64,linux/arm64}
+export IMAGE_NAME=openjdk8:1.0.0-alpine-3.18
+export DOCKERFILE_PATH=./Dockerfile
+export CONTEXT_PATH=.
+
+##################################################################################
+# 下面内容为固定部分
+##################################################################################
+if [[ "${PLATFORMS}" == "*amd64*" ]] && [[ "${PLATFORMS}" == "*arm64*" ]];
+then
+    BUILDER_NAME=multiarch-builder
+elif [[ "${PLATFORMS}" == "*arm64*" ]]; then
+    BUILDER_NAME=arm64-builder
+else
+    BUILDER_NAME=amd64-builder
+fi
+
+# 创建builderx
+if ! docker buildx inspect ${BUILDER_NAME} >/dev/null 2>&1; then
+  docker buildx create --name ${BUILDER_NAME} --platform $PLATFORMS
+fi
 
 # 登录harbor
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin $DOCKER_REGISTRY_URL
+echo "${DOCKER_REGISTRY_PASSWORD}" | docker login -u "${DOCKER_REGISTRY_USERNAME}" --password-stdin $DOCKER_REGISTRY_URL
 
 docker buildx build \
 --sbom=false --provenance=false \
---builder multiarch-builder \
--t $IMAGE_ID \
---platform $PLATFROM \
--f Dockerfile \
---push .
+--builder ${BUILDER_NAME} \
+-t $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_PROJECT/${IMAGE_NAME} \
+--platform ${PLATFORMS} \
+-f ${DOCKERFILE_PATH} \
+--push ${CONTEXT_PATH}
 
 docker logout $DOCKER_REGISTRY_URL
 
