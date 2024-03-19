8e1d11312077d3478a06ec026b64986e42f4df29
lipan <lipan.me@gmail.com>
2024-03-19 11:29:13 +0800

    up

--- doc/images/imageTool/image_sync.sh
+++ doc/images/imageTool/image_sync.sh
@@ -3,13 +3,26 @@
 export SKOPEO_OPTION="--multi-arch all --override-os linux  --insecure-policy --dest-tls-verify=false --dest-username=${DEV_DOCKER_REGISTRY_USERNAME} --dest-password=${DEV_DOCKER_REGISTRY_PASSWORD}"
 
 function image_sync() {
-    IMAGE_SRC=$1
-    IMAGE_DST=$2
+     IMAGE_SRC=$1
+     IMAGE_DST=$2
+
+     if [ "x$IMAGE_DST" == "x" ];then
+      IMAGE_DST=$(echo "$IMAGE_SRC" | awk -F'/' '{print $NF}')
+      IMAGE_DST=${DEV_DOCKER_REGISTRY_URL}/${DEV_DOCKER_REGISTRY_PROJECT}/$IMAGE_DST
+     fi
+
     printf "同步镜像${IMAGE_SRC} => ${IMAGE_DST} 开始....\n"
     eval "skopeo copy  ${SKOPEO_OPTION}  docker://${IMAGE_SRC} docker://${IMAGE_DST}"
     printf "同步镜像${IMAGE_SRC} => ${IMAGE_DST} 完成!\n"
 }
 
-#image_sync curve25519xsalsa20poly1305/openvpn-socks5 ${DEV_DOCKER_REGISTRY_URL}/${DEV_DOCKER_REGISTRY_PROJECT}/openvpn-socks5
-image_sync harbor-dev.fusionfintrade.com:6443/sps/industry-fin-platform:v1.0.0 ${DEV_DOCKER_REGISTRY_URL}/${DEV_DOCKER_REGISTRY_PROJECT}/industry-fin-platform:v1.0.0
-image_sync harbor-dev.fusionfintrade.com:6443/sps/sps-rule-web:v1.0.0 ${DEV_DOCKER_REGISTRY_URL}/${DEV_DOCKER_REGISTRY_PROJECT}/sps-rule-web:v1.0.0
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/nacos-server:v2.1.0
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/nacos-peer-finder-plugin:1.1
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/rabbitmq:3.9.7-management-plugins
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/minio:latest
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/mongo:5.0.2
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/mongo-k8s-sidecar:latest
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/kubernetes-library:kubernetes-zookeeper1.0-3.4.10
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/mysql:5.7
+#image_sync registry.cn-hangzhou.aliyuncs.com/zaqbest/csi:xtrabackup-1.0
+
